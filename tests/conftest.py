796f85d082d6c434c3e84721e306fa92ea9327e8
dilippoudel <dilippoudel578@gmail.com>
2025-03-17 19:35:32 +0200

    Add conftest file for fixture

---
+++ tests/conftest.py
@@ -0,0 +1,50 @@
+"""This file is accessable by all test file """
+
+from fastapi.testclient import TestClient
+import pytest
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from app.main import app
+from app.database import get_db, Base
+from app.config import settings
+
+# SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:1234@localhost:5432/fastapi_test'
+
+SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}_test'
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+"""Defining fixture to clean up the data base before each test case running."""
+@pytest.fixture()
+def session():
+    Base.metadata.drop_all(bind=engine)
+    Base.metadata.create_all(bind=engine)
+    db = TestingSessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@pytest.fixture()
+def client(session):
+    def override_get_db():
+        try:
+            yield session
+        finally:
+            session.close()
+    app.dependency_overrides[get_db] = override_get_db
+    yield TestClient(app)
+
+
+@pytest.fixture()
+def test_user(client):
+    user_data = {"email": "hello123@gmail.com", "password": "password123"}
+    res = client.post("/users", json=user_data)
+    assert res.status_code == 201
+    print(res.json())
+    new_user = res.json()
+    new_user['password'] = user_data["password"]
+    return new_user
